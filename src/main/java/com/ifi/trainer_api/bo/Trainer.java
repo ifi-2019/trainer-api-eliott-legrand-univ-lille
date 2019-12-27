324f5eeb44fb51b4223d5311f81694d46b195a7d
Eliott Legrand <e-legrand59@hotmail.fr>
2019-12-27 18:12:30 +0100

    add password

--- src/main/java/com/ifi/trainer_api/bo/Trainer.java
+++ src/main/java/com/ifi/trainer_api/bo/Trainer.java
@@ -1,9 +1,9 @@
 package com.ifi.trainer_api.bo;
 
+import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import java.util.List;
 
 @Entity
@@ -14,6 +14,9 @@ public class Trainer {
 
     @ElementCollection
     private List<Pokemon> team;
+
+    @Column
+    private String password;
 
     public Trainer() {
     }
@@ -37,5 +40,14 @@ public class Trainer {
 
     public void setTeam(List<Pokemon> team) {
         this.team = team;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
